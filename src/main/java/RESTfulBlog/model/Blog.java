df34a15cd9cdc4c19ad079482f17e64f9e946186
habioye <78454514+habioye@users.noreply.github.com>
2024-06-18 00:19:06 -0500

    put the getter and setter methods

--- src/main/java/RESTfulBlog/model/Blog.java
+++ src/main/java/RESTfulBlog/model/Blog.java
@@ -1,13 +1,17 @@
 package RESTfulBlog.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Blog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +19,30 @@ public class Blog {
     private String name;
     private String description;
 
+    public Blog(String name, String description) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
